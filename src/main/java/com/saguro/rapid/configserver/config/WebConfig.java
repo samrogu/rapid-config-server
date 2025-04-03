f1588b83420bd04f3158ff0fbfa4d9fd39f56c88
Samuel Rodriguez <grd.rodrigez13@gmail.com>
2025-04-02 23:57:11 -0600

    Add cors and id pathvariable

---
+++ src/main/java/com/saguro/rapid/configserver/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.saguro.rapid.configserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas las rutas
+                        .allowedOrigins("http://localhost:3000", "https://example.com") // Dominios permitidos
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                        .allowedHeaders("*") // Permitir todos los encabezados
+                        .allowCredentials(true); // Permitir credenciales (cookies, etc.)
+            }
+        };
+    }
+}
